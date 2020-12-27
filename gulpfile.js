625d33dc64f4aa030bfd5ec5d2c403c24f74ead5
Will Gorman <will.gorman@joyent.com>
2020-12-27 15:33:59 -0600

    Remove syntax highlighting and update gulp dependency

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,11 @@ var gulp = require('gulp')
 	minifyCSS = require('gulp-minify-css');
 
 gulp.task('compress', function() {
-  gulp.src(['assets/js/jquery.min.js', 'assets/js/jquery.prettysocial.min.js', 'assets/js/rainbow-custom.min.js', 'assets/js/scripts.js'])
+  gulp.src(['assets/js/jquery.min.js', 'assets/js/jquery.prettysocial.min.js', 'assets/js/scripts.js'])
     .pipe(concat('all.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('static/js/'));
-  gulp.src(['assets/css/blog.css', 'assets/css/syntax-highlighter.css', 'assets/css/custom.css'])
+  return gulp.src(['assets/css/blog.css', 'assets/css/custom.css'], {allowEmpty: true})
     .pipe(concat('all.min.css'))
     .pipe(minifyCSS())
     .pipe(gulp.dest('static/css/'));
